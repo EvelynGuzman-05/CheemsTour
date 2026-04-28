695ca3d920acd6ceeee44051bbfdba935701c1d8
EvelynGuzman-05 <evelin21guzace@gmail.com>
2026-04-28 12:02:25 -0700

    Proyecto CheemsTour que integra la API de Google Maps junto con las librerías Retrofit y Gson Converter

---
+++ app/src/main/java/mx/itson/cheemstour/entities/Trip.kt
@@ -0,0 +1,14 @@
+package mx.itson.cheemstour.entities
+
+class Trip {
+    constructor(id: Int, name: String, city: String, latitude: Double, longitude: Double){
+
+    }
+
+    var id: Int? = null
+    var name: String? = null
+    var city: String? = null
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+
+}
---
+++ app/src/main/java/mx/itson/cheemstour/utils/RetrofitUtil.kt
@@ -0,0 +1,19 @@
+package mx.itson.cheemstour.utils
+
+import com.google.gson.GsonBuilder
+import mx.itson.cheemstour.interfaces.CheemsAPI
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitUtil {
+
+    fun getApi() : CheemsAPI{
+        val gson = GsonBuilder().create()
+
+        val retrofit = Retrofit.Builder().baseUrl("http://10.232.19.123:5000/").addConverterFactory(
+            GsonConverterFactory.create(gson)).build()
+
+        return retrofit.create(CheemsAPI::class.java)
+    }
+
+}
